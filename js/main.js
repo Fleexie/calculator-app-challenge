d373569807d26cecb73d92e56a56e10d0953965c
Fleexie <jannickolsendk@hotmail.com>
2022-03-18 19:57:16 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function addition(num1,num2){
+    return parseInt(num1) + parseInt(num2)
+}
+function subtraction(num1,num2){
+    return parseInt(num1) - parseInt(num2)
+}
+function multiplication(num1,num2){
+    return parseInt(num1) * parseInt(num2)
+}
+function division(num1,num2){
+    return parseInt(num1) / parseInt(num2)
+}
+
+function calc(){
+
+}
